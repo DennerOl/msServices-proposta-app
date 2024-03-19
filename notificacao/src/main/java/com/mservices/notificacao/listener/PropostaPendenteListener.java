a43f6cec80405318ff001893b9f8834717bea100
DennerOl <dener155@hotmail.com>
2024-03-18 22:14:24 -0300

    Montando a primeira notificação

--- notificacao/src/main/java/com/mservices/notificacao/listener/PropostaPendenteListener.java
+++ notificacao/src/main/java/com/mservices/notificacao/listener/PropostaPendenteListener.java
@@ -3,12 +3,20 @@ package com.mservices.notificacao.listener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import com.mservices.notificacao.constante.MensagemConstante;
+import com.mservices.notificacao.domain.Proposta;
+import com.mservices.notificacao.service.NotificacaoSnsService;
+
 @Component
 public class PropostaPendenteListener {
 
-  @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
-  public void propostaPendente() {
+  private NotificacaoSnsService notificacaoSnsService;
 
+  @RabbitListener(queues = "${rabbitmq.queue.proposta.pendente}")
+  public void propostaPendente(Proposta proposta) {
+
+    String mensagem = String.format(MensagemConstante.PROPOSTA_EM_ANALISE, proposta.getUsuario().getNome());
+    notificacaoSnsService.notificar(mensagem);
   }
 
 }
