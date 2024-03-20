7621899c4c71a6ccdb14ddf7de3a81460221565c
DennerOl <dener155@hotmail.com>
2024-03-19 21:53:26 -0300

    Classe config AWS

--- notificacao/src/main/java/com/mservices/notificacao/service/NotificacaoSnsService.java
+++ notificacao/src/main/java/com/mservices/notificacao/service/NotificacaoSnsService.java
@@ -1,11 +1,22 @@
 package com.mservices.notificacao.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.model.PublishRequest;
 
 @Service
 public class NotificacaoSnsService {
 
-  public void notificar(String mensagem) {
+  @Autowired
+  private AmazonSNS amazonSNS;
+
+  public void notificar(String telefone, String mensagem) {
+
+    PublishRequest publishRequest = new PublishRequest().withMessage(mensagem)
+        .withPhoneNumber(telefone);
+    amazonSNS.publish(publishRequest);
 
   }
 }
