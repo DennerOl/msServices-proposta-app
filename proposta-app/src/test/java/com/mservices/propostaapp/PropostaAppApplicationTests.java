37120f2e63d4c2f9ee4a90cd3f56330924d63523
DennerOl <dener155@hotmail.com>
2024-03-29 12:37:01 -0300

    DockerFile e DockerCompose

--- proposta-app/src/test/java/com/mservices/propostaapp/PropostaAppApplicationTests.java
+++ proposta-app/src/test/java/com/mservices/propostaapp/PropostaAppApplicationTests.java
@@ -1,13 +1,12 @@
 package com.mservices.propostaapp;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PropostaAppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	/*
+	 * @Test
+	 * void contextLoads() {
+	 * }
+	 */
 }
